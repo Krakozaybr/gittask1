db9482b67c99b88401ac18c413a2d905c8eefc56
Krakozaybr <krakozaybp@gmail.com>
2022-12-20 13:52:04 +0700

    task2

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
-import os.path
 import sys
 import random
+import sys
 
-from PyQt5 import QtMultimedia
-from PyQt5.QtCore import QRegExp, Qt
-from PyQt5.QtGui import QRegExpValidator, QPainter, QColor
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import *
-from PyQt5 import QtCore
-from PyQt5 import uic
 
 
-class Solution(QMainWindow):
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(541, 502)
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(220, 230, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "Кнопка"))
+
+
+class Solution(QMainWindow, Ui_Form):
     def __init__(self):
         super().__init__()
         self.central_widget = QWidget()
-        uic.loadUi(os.path.join(os.path.dirname(__file__), 'UI.ui'), self)
+        self.setupUi(self)
         self.circles = []
         self.init_ui()
 
@@ -26,9 +40,9 @@ class Solution(QMainWindow):
         qp = QPainter()
         # Начинаем процесс рисования
         qp.begin(self)
-        qp.setBrush(QColor(255, 255, 0))
 
-        for x, y, r in self.circles:
+        for x, y, r, color in self.circles:
+            qp.setBrush(color)
             qp.drawEllipse(x, y, 2 * r, 2 * r)
 
         # Завершаем рисование
@@ -38,7 +52,18 @@ class Solution(QMainWindow):
         r = random.randint(10, min(self.width(), self.height()) // 2)
         x = random.randint(0, self.width() - r * 2)
         y = random.randint(0, self.height() - r * 2)
-        self.circles.append((x, y, r))
+        self.circles.append(
+            (
+                x,
+                y,
+                r,
+                QColor(
+                    random.randint(0, 255),
+                    random.randint(0, 255),
+                    random.randint(0, 255)
+                )
+            )
+        )
         self.repaint()
 
 
